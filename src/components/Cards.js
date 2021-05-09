4378e6972fbf1e71891b846f954549151b5df3b3
16-sanalmishra <sanal.m1600@gmail.com>
2021-05-09 13:17:31 +0530

    updated

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -10,31 +10,31 @@ function Cards() {
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <CardItem
-              src='https://analyticsindiamag.com/wp-content/uploads/2019/12/How-SMBs-In-India-Can-Unlock-Their-Potential-With-Analytics.jpg'
+              src='https://images.hindustantimes.com/rf/image_size_630x354/HT/p2/2018/07/01/Pictures/_d54e1ff0-7d19-11e8-98f1-ed5cda4a01f0.jpg'
               text='I am a business owner at Pottery Us and HawkerIn helps me to sell my products at a large scale of customers. Thank you HawkerIn.'
               path=''
             />
             <CardItem
-              src='https://images.unsplash.com/photo-1587127964224-ee5b0a61933d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGJhemFhcnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+              src='https://assets.seniority.in/media/ktpl_blog/Yoga_Asanas_That_Anyone_Can_Do-main_image.jpg'
               text='We at Clothes For You, sell clothes of different cultures of India. We are thankful to HawkerIn for providing us this platform. I am truely satisfied.'
               path=''
             />
           </ul>
           <ul className='cards__items'>
             <CardItem
-              src='https://i2.wp.com/ttim.photo/wp-content/uploads/2016/12/Closeup-of-Young-Woman-at-Fortress-Gate-Jodhpur-India-Copyright-2016-Ralph-Velasco.jpg?resize=750%2C500'
+              src='https://media.istockphoto.com/photos/portrait-of-handsome-latino-african-man-picture-id1007763808?k=6&m=1007763808&s=612x612&w=0&h=Js1VDBulbaNw_CF7fghP_nhUPCC-DQTqb7Wym1CdTOI='
               text='I am using HawkerIn from past 6 months and I have seen a huge surge in my sales. I am tuely satisfied.'
               label='Review 1'
               path=''
              />
             <CardItem
-              src='https://miro.medium.com/max/625/1*ltUNWd5KLkaZB8bIrUxQ1Q.jpeg'
+              src='https://thumbs.dreamstime.com/b/fat-happy-man-pleased-himself-proper-nutrition-weight-loss-131695038.jpg'
               text='Thank you HawkerIn for provideing us this platform to sale our stuff directly to customers. I am a satisfied user.'
               label='Review 2'
               path=''
             />
             <CardItem
-              src='https://media.gettyimages.com/photos/young-woman-selling-clothes-at-a-shopping-mall-picture-id471760764?s=612x612'
+              src='https://thumbs.dreamstime.com/b/grateful-happy-african-man-holding-hands-chest-feeling-appreciation-pleased-to-heart-love-gratitude-honesty-thankful-sincere-138433528.jpg'
               text='I am supporting my family business by registering it in HawkerIn. I can now dirctly rech to customers and sell my products. I am highly satisfied.'
               label='Review 3'
               path=''
--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -11,47 +11,38 @@ export default function Services() {
         <h2>Collaborators Details</h2>
         <ul class="list-unstyled">
           <li class="media my-5">
-            <img class="mr-3 align-self-center" src="https://i.pinimg.com/originals/a7/21/8d/a7218d58c8873f6b9bb917cbce0116e7.jpg" style={{height:150,width:150}} alt="Generic placeholder image"/>
+            <img class="mr-3 align-self-center" src="https://mail.google.com/mail/u/1/#inbox/FMfcgxwLtsvNzPRCgKWszPkxcsRqbKTn?projector=1&messagePartId=0.1" style={{height:150,width:150}} alt="Generic placeholder image"/>
             <div class="media-body align-self-center">
-              <h5 class="mt-0 mb-1">Akshita Saxena</h5>
-              E-mail address  :  akshita.saxena_cs18@gla.ac.in<br></br>
-              Github Username :  Akshitasaxena22
+              <h5 class="mt-0 mb-1">Sanal Mishra</h5>
+              E-mail address  :  sanal.mishra_cs18@gla.ac.in<br></br>
+              
             </div>
           </li>
           <hr></hr>
           <li class="media my-5">
             <img class="mr-3 align-self-center" src="https://i.pinimg.com/originals/a7/21/8d/a7218d58c8873f6b9bb917cbce0116e7.jpg" style={{height:150,width:150}}  alt="Generic placeholder image"/>
             <div class="media-body align-self-center">
-              <h5 class="mt-0 mb-1">Ashita Goyal</h5>
-              E-mail address :  ashita.goyal_cs18@gla.ac.in<br></br>
-              Github Username :  ashitag64
+              <h5 class="mt-0 mb-1">Sarvendra Kumar Singh</h5>
+              E-mail address :  sarvendra.kumar_cs18@gla.ac.in<br></br>
+              
             </div>
           </li>
           <hr></hr>
           <li class="media my-5">
             <img class="mr-3 align-self-center" src="https://i.pinimg.com/originals/a7/21/8d/a7218d58c8873f6b9bb917cbce0116e7.jpg" style={{height:150,width:150}}  alt="Generic placeholder image"/>
             <div class="media-body align-self-center">
-              <h5 class="mt-0 mb-1">Bharti Gautam.k</h5>
-              E-mail address :  bharti.gautam_cs18@gla.ac.in<br></br>
-              Github Username :  bgautam9045
+              <h5 class="mt-0 mb-1">Prabhat Kumar Singh</h5>
+              E-mail address :  prabhat.kumar_cs18@gla.ac.in<br></br>
+              
             </div>
           </li>
           <hr></hr>
           <li class="media my-5">
             <img class="mr-3 align-self-center" src="https://i.pinimg.com/originals/a7/21/8d/a7218d58c8873f6b9bb917cbce0116e7.jpg" style={{height:150,width:150}}  alt="Generic placeholder image"/>
             <div class="media-body align-self-center">
-              <h5 class="mt-0 mb-1">Naina Agrawal</h5>
-              E-mail address :  naina.agrawal_cs18@gla.ac.in<br></br>
-              Github Username :  naina2910
-            </div>
-          </li>
-          <hr></hr>
-          <li class="media my-5">
-            <img class="mr-3 align-self-center" src="https://i.pinimg.com/originals/a7/21/8d/a7218d58c8873f6b9bb917cbce0116e7.jpg" style={{height:150,width:150}} alt="Generic placeholder image"/>
-            <div class="media-body align-self-center">
-              <h5 class="mt-0 mb-1">Nandani Bansal</h5>
-              E-mail address :  nandani.bansa_cs18@gla.ac.in<br></br>
-              Github Username :  nandani7
+              <h5 class="mt-0 mb-1">Yash Kumar Singh</h5>
+              E-mail address :  yash.kumar_cs18@gla.ac.in<br></br>
+              
             </div>
           </li>
         </ul>
